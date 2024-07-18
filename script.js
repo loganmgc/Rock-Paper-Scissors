1516dc7ac99a51817d51440a3d604b469c49cab4
Guney <guneycin@gmail.com>
2024-07-18 18:10:04 +0300

    general improvements to the code
    Changes were made to make the code more readable. Class, id and variable names have been edited, functions have been written for repetitive codes and minor edits

--- script.js
+++ script.js
@@ -1,50 +1,46 @@
 const moves = ['rock', 'paper', 'scissors'];
-const randommoves = [0, 1, 2]
 const image = [
     `img/rock.png`,
     `img/paper.png`,
     `img/scissors.png`,
 ]
 const resultmsgs = ['DRAW', 'YOU WIN', 'YOU LOSE'];
-let computerMoveImage = document.getElementById('oppenentsMove');
-let playerMoveImage = document.getElementById('yourMove');
-let resultmsg = document.getElementById('result');
-let displayresult = document.getElementById('resutlmsg');
-function MainGame(playerChoice, type) {
+const resultmsg = document.getElementById('result');
+const displayResult = document.getElementById('resutlmsg');
+function MainGame(playerChoice) {
     resultmsg.innerText = '';
-    displayresult.style.opacity = 0;
-    let button = document.getElementsByClassName('button');
-    let player = document.getElementById(playerChoice).value;
-    if (type == 1) {
-        for (var i = 0; i < button.length; i++) {
-            button[i].setAttribute('disabled', true);
-        }
+    displayResult.style.opacity = 0;
+    const button = document.getElementsByClassName('button');
+    for (var i = 0; i < button.length; i++) {
+        button[i].setAttribute('disabled', true);
     }
-    playerMoveImage.style.backgroundImage = `url('${image[player]}')`;
-    playerMoveImage.style.opacity = 0.9;
-    let moveIndex = Math.floor(Math.random() * 3)
-    let computer = moves[moveIndex];
+    const playerMove = document.getElementById(playerChoice).value;
+    displayMove('playerMoveImage', playerMove);
+    const computerMove = Math.floor(Math.random() * 3);
     let index = 0;
-    var result = (CheckWin(player, computer));
     const interval = setInterval(() => {
-        computerMoveImage.style.opacity = 0.9;
-        computerMoveImage.style.backgroundImage = `url(${image[randommoves[index]]})`;
         index = (index + 1) % image.length;
+        displayMove('computerMoveImage', index);
     }, 75);
     setTimeout(() => {
         clearInterval(interval);
-        computerMoveImage.style.backgroundImage = `url(${image[moveIndex]})`;
-        resultmsg.innerText = resultmsgs[result];
+        displayMove('computerMoveImage', computerMove);
+        resultmsg.innerText = resultmsgs[determineWinner(playerMove, computerMove)];
         for (var i = 0; i < button.length; i++) {
             button[i].disabled = false;
         }
-        displayresult.style.opacity = 1;
+        displayResult.style.opacity = 1;
     }, 1500);
 }
-function CheckWin(playerMove, computerMove) {
-    if (moves[playerMove] == moves[0] && computerMove == moves[2] ||
-        moves[playerMove] == moves[1] && computerMove == moves[0] ||
-        moves[playerMove] == moves[2] && computerMove == moves[1]) return 1;
-    else if (moves[playerMove] == computerMove) return 0;
+function displayMove(element, move) {
+    const e = document.getElementById(element);
+    e.style.backgroundImage = `url('${image[move]}')`;
+    e.style.opacity = 0.9;
+}
+function determineWinner(playerMove, computerMove) {
+    if (moves[playerMove] == moves[0] && moves[computerMove] == moves[2] ||
+        moves[playerMove] == moves[1] && moves[computerMove] == moves[0] ||
+        moves[playerMove] == moves[2] && moves[computerMove] == moves[1]) return 1;
+    else if (moves[playerMove] == moves[computerMove]) return 0;
     else return 2;
 }
